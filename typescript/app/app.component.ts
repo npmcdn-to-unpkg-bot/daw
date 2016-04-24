8c23c62b5a3f0c0485f6346ea9c3b2d227a23b77
Pablo <pablo@mylaif.es>
2016-04-24 21:46:53 +0200

    Revert "LogIn+SignIn"
    This reverts commit f25139c7115d812f6dc9dee9e0d7a5e8282bb866.

--- typescript/app/app.component.ts
+++ typescript/app/app.component.ts
@@ -16,17 +16,13 @@ import {PerfilService} from './perfil.service';
 //Index
 import {IndexService} from './index.service';
 import {IndexListComponent} from './index-list.component';
-//Login
-import {LogInService} from './login.service';
-import {LogInDetailComponent} from './login-detail.component';
-
 
 @Component({
   selector: 'app',
   template: `
     <router-outlet></router-outlet>
   `,
-  providers:  [RestauranteService, RecetaService,LogInService],
+  providers:  [RestauranteService, RecetaService],
   directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
@@ -44,8 +40,6 @@ import {LogInDetailComponent} from './login-detail.component';
   {path: '/receta/edit/:id', name: 'RecetaEdit', component: RecetaFormComponent}
   //PerfilPrivado
   {path: '/perfil', name: 'Perfil', component: PerfilDetailComponent}
-//Login
-    {path:'/login', name: 'LogIn', component: LogInDetailComponent}
 ])
-
+])
 export class AppComponent { }
