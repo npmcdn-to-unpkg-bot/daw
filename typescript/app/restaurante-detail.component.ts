dddb119d42584686556548ce0e27364156dd6246
ilopez <Nacho@MacBook-Pro-de-Ignacio-2.local>
2016-04-25 21:07:13 +0200

    Restaurantes relleno

--- typescript/app/restaurante-detail.component.ts
+++ typescript/app/restaurante-detail.component.ts
@@ -13,14 +13,13 @@ import {Restaurante, RestauranteService}   from './restaurante.service';
             <h2 class="text-center">{{restaurante.title}}</h2>
             <button (click)="editRestaurante()" type="submit" class="btn btn-default publicar">Editar</button>
             <button (click)="removeRestaurante()" type="submit" class="btn btn-default publicar">Eliminar</button>
-            <div class="contenido" [innerHtml]="restaurante.details">
-              <div class="row">
+            <div class="contenido" [innerHtml]="restaurante.details"></div>
+            <div class="row">
                 <div class="col-md-12">
                     <h3>Localización</h3>
                     <iframe src="{{restaurante.map}}" width="100%" height="480"></iframe>
                 </div>
-              </div>
-             </div>
+            </div>
           </div>
         </div>
       </div>
